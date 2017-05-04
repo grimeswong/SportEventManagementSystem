c482f0a73b17d0e62f9e8601da6dc5fb3406d836
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-05-04 16:02:54 +1000

    Created data model for registering a user with validation - needs testing

--- SportEventManagementSystem/SportEventManagementSystem/Models/AccountViewModels/RegisterViewModel.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/AccountViewModels/RegisterViewModel.cs
@@ -14,6 +14,76 @@ namespace SportEventManagementSystem.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
+        [StringLength(35, ErrorMessage = "Your {0} must be at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(35, ErrorMessage = "Your {0} must be at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(35, ErrorMessage = "Your {0} must be at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [StringLength(60, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Street")]
+        public string Street { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Suburb")]
+        public string Suburb { get; set; }
+
+        [Required]
+        [StringLength(4, ErrorMessage = "Please enter a valid postcode. {1} characters long.", MinimumLength = 4)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Postcode")]
+        public string PostCode { get; set; }
+
+        [Required]
+        [DataType("Gender")]
+        [Display(Name = "Gender")]
+        [IsGender(ErrorMessage = "Please select a gender.")]
+        public int Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"/^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$/",ErrorMessage = "Not a valid phone number.")]
+        [Display(Name = "Home Phone")]
+        public int HomePhone { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"/^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$/", ErrorMessage = "Not a valid phone number.")]
+        [Display(Name = "Mobile Phone")]
+        public int MobilePhone { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(70, ErrorMessage = "{0} must be at max {1} character long.", MinimumLength = 1)]
+        [Display(Name = "Emergency Contact Name")]
+        public int EmergencyContact { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"/^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$/", ErrorMessage = "Not a valid phone number.")]
+        [Display(Name = "Emergency Contact Number")]
+        public int EmergencyContactNo { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
@@ -23,5 +93,20 @@ namespace SportEventManagementSystem.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class IsGender : ValidationAttribute
+    {
+        private const string defaultError = "'{0}' must have at least one element.";
+        public IsGender() : base(defaultError) //
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            int v = (int)value;
+            return (v > 0 && v < 2) || false;
+        }
     }
 }
