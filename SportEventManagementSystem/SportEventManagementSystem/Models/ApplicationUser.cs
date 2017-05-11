b0d970e36610173d44178fb4457aa79d7ccecafa
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-05-11 16:58:21 +1000

    Properly insert register data into account

--- SportEventManagementSystem/SportEventManagementSystem/Models/ApplicationUser.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/ApplicationUser.cs
@@ -9,5 +9,17 @@ namespace SportEventManagementSystem.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; internal set; }
+        public string EmergencyContactNo { get; internal set; }
+        public string EmergencyContact { get; internal set; }
+        public string MobilePhone { get; internal set; }
+        public string HomePhone { get; internal set; }
+        public string MiddleName { get; internal set; }
+        public string LastName { get; internal set; }
+        public DateTime DateOfBirth { get; internal set; }
+        public string Street { get; internal set; }
+        public string Suburb { get; internal set; }
+        public int Gender { get; internal set; }
+        public string PostCode { get; internal set; }
     }
 }
