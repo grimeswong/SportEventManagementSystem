aebc8aba8f0bbabe9f923c85a0cb62ea964d872f
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-05-11 17:01:42 +1000

    Removed code blocks we won't be using in this project left over from auto-generated code

--- SportEventManagementSystem/SportEventManagementSystem/Models/ManageViewModels/IndexViewModel.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/ManageViewModels/IndexViewModel.cs
@@ -10,11 +10,7 @@ namespace SportEventManagementSystem.Models.ManageViewModels
     {
         public bool HasPassword { get; set; }
 
-        public IList<UserLoginInfo> Logins { get; set; }
-
         public string PhoneNumber { get; set; }
-
-        public bool TwoFactor { get; set; }
 
         public bool BrowserRemembered { get; set; }
     }
