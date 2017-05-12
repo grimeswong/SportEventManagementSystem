140acde10b9b93aaa18ca0d6dbeb0d97f1881863
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-05-12 19:56:27 +1000

    Basic change details form for user

--- SportEventManagementSystem/SportEventManagementSystem/Models/ManageViewModels/IndexViewModel.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/ManageViewModels/IndexViewModel.cs
@@ -8,9 +8,19 @@ namespace SportEventManagementSystem.Models.ManageViewModels
 {
     public class IndexViewModel
     {
-        public bool HasPassword { get; set; }
-
-        public string PhoneNumber { get; set; }
+        public string Email { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string EmergencyContactNo { get; internal set; }
+        public string EmergencyContact { get; internal set; }
+        public string MobilePhone { get; internal set; }
+        public string HomePhone { get; internal set; }
+        public string MiddleName { get; internal set; }
+        public string LastName { get; internal set; }
+        public DateTime DateOfBirth { get; internal set; }
+        public string Street { get; internal set; }
+        public string Suburb { get; internal set; }
+        public int Gender { get; internal set; }
+        public string PostCode { get; internal set; }
 
         public bool BrowserRemembered { get; set; }
     }
