a7259bc4b5362cb0a0cca0c2a01cd13b030e5c8c
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-05-12 20:10:15 +1000

    Barebones Event model - not finished

---
+++ SportEventManagementSystem/SportEventManagementSystem/Models/ApplicationEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SportEventManagementSystem.Models
+{
+    public class ApplicationEvent : IdentityUser<int>
+    {
+        //INSERT STUFF HERE
+        public List<Competition> Competitions = new List<Competition>();
+        
+    }
+
+    public class Competition
+    {
+        public Division Division;
+       // public List<Team,>Competition Participants = new List<Participants>();
+    }
+
+    public class Division
+    {
+        public string DivisionName;
+        public string DivisionDescription;
+    }
+
+    public class Participants
+    {
+        //public
+    }
+
+    public class SportType
+    {
+
+    }
+}
