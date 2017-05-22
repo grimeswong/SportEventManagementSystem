b15e071f3de84575387f8330630a15770d68fbb5
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-05-22 17:21:30 +1000

    Add temporary functionality for create event form in preparation for presentation

---
+++ SportEventManagementSystem/SportEventManagementSystem/Controllers/EventController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SportEventManagementSystem.Models;
+using SportEventManagementSystem.Models.AccountViewModels;
+using SportEventManagementSystem.Services;
+
+namespace SportEventManagementSystem.Controllers
+{
+    [Authorize]
+    public class EventController : Controller
+    {
+
+        public EventController()
+        {
+        }
+
+        //
+        // GET: /Event/CreateEvent
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Create(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        ////
+        //// POST: /Create/Event
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Login(EventViewModel model, string returnUrl = null)
+        //{
+        //    ViewData["ReturnUrl"] = returnUrl;
+        //    if (ModelState.IsValid)
+        //    {
+        //        // This doesn't count login failures towards account lockout
+        //        // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+        //        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+        //        if (result.Succeeded)
+        //        {
+        //            _logger.LogInformation(1, "User logged in.");
+        //            return RedirectToLocal(returnUrl);
+        //        }
+        //        if (result.IsLockedOut)
+        //        {
+        //            _logger.LogWarning(2, "User account locked out.");
+        //            return View("Lockout");
+        //        }
+        //        else
+        //        {
+        //            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        //            return View(model);
+        //        }
+        //    }
+
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SportEventManagementSystem/SportEventManagementSystem/Models/EventViewModels/CreateEventViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SportEventManagementSystem.Models.EventViewModels
+{
+    public class CreateEventViewModel
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Your {0} must be at max {1} character long", MinimumLength = 1)]
+        [Display(Name = "Event Name")]
+        [DataType(DataType.Text)]
+        public string EventName { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Your {0} must be at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Event Venue Name")]
+        public string EventVenueName { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Street")]
+        public string EventStreetName { get; set; }
+
+        [Required]
+        [StringLength(35, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Suburb")]
+        public string Suburb { get; set; }
+
+        [Required]
+        [StringLength(4, ErrorMessage = "Please enter a valid postcode. {1} characters long.", MinimumLength = 4)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Postcode")]
+        public string PostCode { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Event Start Date")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yy}")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Event End Date")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yy}")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Registration Start Date & Time")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yy H:mm:ss zzz}")] // need to set the TimeSpan.Ten (option for the eastern and western time zone)
+        public DateTime RegStartDateTime { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Registration End Date & Time")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yy H:mm:ss zzz}")] // need to set the TimeSpan.Ten (option for the eastern and western time zone)
+        public DateTime RegEndDateTime { get; set; }
+
+        [Required]
+        [Range(1, 100000)]    // Range from 1 to 100000 (need to see work or not) or need to use custom one
+        [Display(Name = "Entry Capacity")]
+        public int EntryCapacity { get; set; }
+
+
+        [StringLength(70, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Organiser Name")]
+        public string OrganiserName { get; set; }
+
+        [StringLength(70, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Organiser Club")]
+        public string OrganiserClub { get; set; }
+
+    }
+
+}
