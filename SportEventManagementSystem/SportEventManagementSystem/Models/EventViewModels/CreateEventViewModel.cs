cb2f8e3c473a1f5cf20552863f66e6c654f55c54
shadowgrimes <grimeswong@gmail.com>
2017-07-11 21:50:40 +1000

    Modified Create Event Page to match design documents

--- SportEventManagementSystem/SportEventManagementSystem/Models/EventViewModels/CreateEventViewModel.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/EventViewModels/CreateEventViewModel.cs
@@ -13,19 +13,25 @@ namespace SportEventManagementSystem.Models.EventViewModels
         [StringLength(30, ErrorMessage = "Your {0} must be at max {1} character long", MinimumLength = 1)]
         [Display(Name = "Event Name")]
         [DataType(DataType.Text)]
-        public string EventName { get; set; }
+        public string Name { get; set; }
 
         [Required]
         [StringLength(30, ErrorMessage = "Your {0} must be at max {1} character long.", MinimumLength = 1)]
         [DataType(DataType.Text)]
         [Display(Name = "Event Venue Name")]
-        public string EventVenueName { get; set; }
+        public string VenueName { get; set; }
+
+        [Required]
+        [StringLength(300, ErrorMessage = "Your {0} must be at max {1} character long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Event Description")]
+        public string Description { get; set; }
 
         [Required]
         [StringLength(30, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
         [DataType(DataType.Text)]
         [Display(Name = "Street")]
-        public string EventStreetName { get; set; }
+        public string StreetAddress { get; set; }
 
         [Required]
         [StringLength(35, ErrorMessage = "Your {0} must be at least {2} and at max {1} character long.", MinimumLength = 1)]
@@ -43,25 +49,25 @@ namespace SportEventManagementSystem.Models.EventViewModels
         [DataType(DataType.Date)]
         [Display(Name = "Event Start Date")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yy}")]
-        public DateTime StartDate { get; set; }
+        public DateTime StartTime { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         [Display(Name = "Event End Date")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yy}")]
-        public DateTime EndDate { get; set; }
+        public DateTime EndTime { get; set; }
 
         [Required]
         [DataType(DataType.DateTime)]
         [Display(Name = "Registration Start Date & Time")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yy H:mm:ss zzz}")] // need to set the TimeSpan.Ten (option for the eastern and western time zone)
-        public DateTime RegStartDateTime { get; set; }
+        public DateTime RegStartTime { get; set; }
 
         [Required]
         [DataType(DataType.DateTime)]
         [Display(Name = "Registration End Date & Time")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yy H:mm:ss zzz}")] // need to set the TimeSpan.Ten (option for the eastern and western time zone)
-        public DateTime RegEndDateTime { get; set; }
+        public DateTime RegEndTime { get; set; }
 
         [Required]
         [Range(1, 100000)]    // Range from 1 to 100000 (need to see work or not) or need to use custom one
