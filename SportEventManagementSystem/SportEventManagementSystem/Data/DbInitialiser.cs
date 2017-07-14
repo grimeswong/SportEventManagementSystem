a5d8717981ecf3375bb31c9061fe66da38c0ce66
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-07-14 10:07:06 +1000

    Add example manual way to add data, also show how 'GenFu' could be used in future to auto-generate data

--- SportEventManagementSystem/SportEventManagementSystem/Data/DbInitialiser.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Data/DbInitialiser.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SportEventManagementSystem.Controllers;
+using SportEventManagementSystem.Models;
+//using GenFu;
 
 namespace SportEventManagementSystem.Data
 {
@@ -16,6 +19,40 @@ namespace SportEventManagementSystem.Data
                 return;   // DB has been seeded
             }
 
+            /*Example to create an event (before comp re-added)
+            List<TeamMember> _teamMembers = new List<TeamMember>();
+
+            _teamMembers.Add(new TeamMember { MemberName = "Member1" });
+            _teamMembers.Add(new TeamMember { MemberName = "Member2" });
+            _teamMembers.Add(new TeamMember { MemberName = "Member3" });
+
+            List<Team> _teams = new List<Team>();
+            var team = new Team
+            {
+                TeamName = "Team1",
+                ManagerID = "423ffb94-50f8-4f65-b306-e5c1faa997af",
+                TeamMembers = _teamMembers,
+            };
+            _teams.Add(team);
+
+            var _participants = new Participants
+            {
+                Teams = _teams
+            };
+            var _event = new Event
+            {
+                id = "tempEvent1",
+                Name = "Test Event 1",
+                Description = "Description event 1",
+                Participants = _participants
+            };
+
+            context.Events.Add(_event); */
+
+            //Looking into using GenFu to generate data
+            //var events = GenFu.GenFu.ListOf<Event>(200);
+
+            //context.Users.Add(new Models.ApplicationUser )
             context.SaveChanges();
         }
     }
