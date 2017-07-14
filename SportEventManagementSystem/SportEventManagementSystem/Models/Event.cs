fa61b592026be950a24eeb7d643cf08d6b9b40d1
Lachlan Horsey <lachlan.horsey@gmail.com>
2017-07-14 10:08:24 +1000

    Make futher changes to event, spelling fix / add full functionality back into data model

--- SportEventManagementSystem/SportEventManagementSystem/Models/Event.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/Event.cs
@@ -12,10 +12,11 @@ namespace SportEventManagementSystem.Models
     {
         [Key]
         public string id { get; set; }
-       // public ICollection<Competition> Competitions { get; set; }
+
+        public List<Competition> Competitions { get; set; }
         public string Name { get; set; }
         public string VenueName { get; set; }
-        public string Desciption { get; set; }
+        public string Description { get; set; }
         public string StreetAddress { get; set; }
         public string Suburb { get; set; }
         public string Postcode { get; set; }
@@ -26,54 +27,62 @@ namespace SportEventManagementSystem.Models
         public int EntryCapacity { get; set; }
         public string OrginiserName { get; set; }
         public string OrginiserClub { get; set; }
-
-        //Temporary until competitions is figured out
-        public List<Participants> Participants { get; set; }
     }
-    //public class Competition
-    //{
-    //    public string Name { get; set; }
-    //    public Division Division { get; set; }
-    //    // TODO public List<Team,>Competition Participants = new List<Participants>();
-    //    public SportType SportType { get; set; }
-    //    public string Location { get; set; }
-    //    public int TeamSizeMin { get; set; }
-    //    public int TeamSizeMax { get; set; }
-    //    public ICollection<Restriction> Restrictions { get; set; }
-    //    public DateTime StartTime { get; set; }
-    //    public DateTime EndTime { get; set; }
-    //    public int EntryCapacity { get; set; }
-    //}
+    public class Competition
+    {
+        [Key]
+        public string id { get; set; }
 
-    //public enum RestrictionTypes
-    //{
-    //    Age,
-    //    Gender
-    //}
+        public string Name { get; set; }
+        public Division Division { get; set; }
+        public Participants Participants = new Participants();
+        public SportType SportType { get; set; }
+        public string Location { get; set; }
+        public int TeamSizeMin { get; set; }
+        public int TeamSizeMax { get; set; }
+        public List<Restriction> Restrictions { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int EntryCapacity { get; set; }
+    }
 
-    //public class Restriction
-    //{
-    //    public RestrictionTypes restrictionType { get; set; }
-    //    public int restrictionValue { get; set; }
+    public enum RestrictionTypes
+    {
+        Age,
+        Gender
+    }
+
+    public class Restriction
+    {
+        [Key]
+        public string id { get; set; }
+
+        public RestrictionTypes restrictionType { get; set; }
+        public int restrictionValue { get; set; }
 
 
-    //    //for (Restriction restriction in Restriction)
-    //    //{
-    //    //    if(restriction.restrictionType == RestrictionTypes.Age)
-    //    //    {
-    //    //        return <currentValue>.restrictionValue <= restrictionValue;
-    //    //    } else if(restriction.restrictionType == RestrictionTypes.Gender)
-    //    //    {
-    //    //        return < currentValue >.restrictionValue == restrictionValue;
-    //    //    }
-    //    //}
-    //}
+        //!!!!!!USE THIS IN OUT CREATE EVENT MODEL TO CHECK FOR VALUE RESTRICTION!!!!!!!!
 
-    //public class Division
-    //{
-    //    public string DivisionName { get; set; }
-    //    public string DivisionDescription { get; set; }
-    //}
+        //for (Restriction restriction in Restriction)
+        //{
+        //    if(restriction.restrictionType == RestrictionTypes.Age)
+        //    {
+        //        return <currentValue>.restrictionValue <= restrictionValue;
+        //    } else if(restriction.restrictionType == RestrictionTypes.Gender)
+        //    {
+        //        return < currentValue >.restrictionValue == restrictionValue;
+        //    }
+        //}
+    }
+
+    public class Division
+    {
+        [Key]
+        public string id { get; set; }
+
+        public string DivisionName { get; set; }
+        public string DivisionDescription { get; set; }
+    }
 
     public class Participants
     {
@@ -91,18 +100,23 @@ namespace SportEventManagementSystem.Models
         public string TeamName { get; set; }
         public string ManagerID { get; set; }
         public List<TeamMember> TeamMembers { get; set; }
+
     }
 
     public class TeamMember
     {
         [Key]
         public string id { get; set; }
+
         public string MemberName { get; set; }
     }
 
-    //public class SportType
-    //{
-    //    public string Name { get; set; }
-    //    public string Description { get; set; }
-    //}
+    public class SportType
+    {
+        [Key]
+        public string id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
 }
