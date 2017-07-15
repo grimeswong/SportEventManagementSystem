c0586076b6fb40b858e27988da18fc23f3a28184
Shadow Grimes <grimes.wong@griffithuni.edu.au>
2017-07-15 20:38:13 +1000

    Modified the Data/DbInitialiser.cs for seeding the fake data (one case) for Event

--- SportEventManagementSystem/SportEventManagementSystem/Data/DbInitialiser.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Data/DbInitialiser.cs
@@ -19,40 +19,109 @@ namespace SportEventManagementSystem.Data
                 return;   // DB has been seeded
             }
 
-            /*Example to create an event (before comp re-added)
-            List<TeamMember> _teamMembers = new List<TeamMember>();
+            /*** Grimes Database ***/
+            /*** Create 3 Events and 3 team (one is Individual) data feeder
 
-            _teamMembers.Add(new TeamMember { MemberName = "Member1" });
-            _teamMembers.Add(new TeamMember { MemberName = "Member2" });
-            _teamMembers.Add(new TeamMember { MemberName = "Member3" });
+            /*** TeamMembers ***/
+            List<TeamMember> _teamMembers = new List<TeamMember>(); //List Array
+            _teamMembers.Add(new TeamMember { MemberName = "Team 1, Member1" });
+            _teamMembers.Add(new TeamMember { MemberName = "Team 1, Member2" });
+            _teamMembers.Add(new TeamMember { MemberName = "Team 1, Member3" });
 
+            /*** Team or Individual Participant ***/
             List<Team> _teams = new List<Team>();
             var team = new Team
             {
-                TeamName = "Team1",
-                ManagerID = "423ffb94-50f8-4f65-b306-e5c1faa997af",
-                TeamMembers = _teamMembers,
+                TeamName = "",
+                ManagerID = "",
+                TeamMembers = _teamMembers
             };
             _teams.Add(team);
 
+            // Participants
             var _participants = new Participants
             {
                 Teams = _teams
             };
-            var _event = new Event
+
+
+
+            /*** division ***/
+            var division = new Division
             {
-                id = "tempEvent1",
-                Name = "Test Event 1",
-                Description = "Description event 1",
-                Participants = _participants
+                DivisionName = "division one",
+                DivisionDescription = "division one description"
             };
 
-            context.Events.Add(_event); */
+            /*** sport type ***/
+            var sportType = new SportType
+            {
+                Name = "soccer",
+                Description = "type 1 description"
+            };
 
-            //Looking into using GenFu to generate data
-            //var events = GenFu.GenFu.ListOf<Event>(200);
+            /*** restriction ***/
+            var _restrictions = new List<Restriction>();
+            var restrictionMin18 = new Restriction
+            {
+                restrictionType = RestrictionTypes.MinimumAge,
+                restrictionValue = 18
+            };
+            var restrictionMax60 = new Restriction
+            {
+                restrictionType = RestrictionTypes.MaximumAge,
+                restrictionValue = 60
+            };
+            _restrictions.Add(restrictionMin18);   //add the Minimum age 18 restriction
+            _restrictions.Add(restrictionMax60);   //add the Maximum age 60 restriction
 
-            //context.Users.Add(new Models.ApplicationUser )
+
+            /*** Competition ***/
+            var _competitions = new List<Competition>();
+            var competition = new Competition
+            {
+
+                Name = "Test Comp 1",
+                Division = division,
+                Participants = _participants,
+                SportType = sportType,
+                Location = "Brisbane",
+                TeamSizeMin = 1,
+                TeamSizeMax = 10,
+                Restrictions = _restrictions,
+                StartTime = new DateTime(17, 10, 01, 0, 0, 0), // 1-Oct-17 00:00:00
+                EndTime = new DateTime(17, 10, 04, 23, 59, 59), // 4-Oct-17 23:59:59
+                EntryCapacity = 100
+            };
+            _competitions.Add(competition);   //Add the first set of competition
+
+
+            /*** Event ***/
+            var _event = new Event
+            {
+                Competitions = _competitions,
+                Name = "Test Event 1",
+                VenueName = "Sunrise Stadium",
+                Description = "Description event 1",
+                StreetAddress = "188 GoodView St",
+                Suburb = "Good View",
+                Postcode = "4000",
+                StartTime = new DateTime(17, 10, 02, 15, 0, 0), // 1-Oct-17 15:00:00
+                EndTime = new DateTime(17, 10, 02, 18, 0, 0), // 1-Oct-17 18:00:00
+                RegStartTime = new DateTime(17, 08, 01, 0, 0, 0), // 1-Aug-17 00:00:00
+                RegEndTime = new DateTime(17, 08, 10, 23, 59, 59), // 1-Aug-17 23:59:59
+                EntryCapacity = 100,
+                OrginiserName = "Alan Smith",
+                OrginiserClub = "Aspley Rugby Club"
+            };
+
+            context.Events.Add(_event);
+
+            // Looking into using GenFu to generate Data
+            // var evnets GenFu.GenFu.ListOf<Event>(200);
+
+            //context.Users.Add(new Models.ApplicationUser);
+
             context.SaveChanges();
         }
     }
