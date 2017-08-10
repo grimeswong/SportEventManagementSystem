225a889a94f8d6b873caf0aa73937bb8306b513a
Lachlan H <lachlan.horsey@gmail.com>
2017-08-10 20:09:31 +1000

    Add forgotten file to repo necessary for searching

---
+++ SportEventManagementSystem/SportEventManagementSystem/Models/EventViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportEventManagementSystem.Models.EventViewModels
+{
+    public class SearchViewModel
+    {
+        public string param { get; set; }
+
+        public List<Event> results { get; set; }
+
+    }
+}
