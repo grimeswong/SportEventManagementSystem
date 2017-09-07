08eb8c3a996cb4a4b3214da85cff616d4558fee9
Lachlan H <lachlan.horsey@gmail.com>
2017-09-07 15:42:28 +1000

    Add validation for join competition view model

--- SportEventManagementSystem/SportEventManagementSystem/Models/EventViewModels/JoinEventViewModel.cs
+++ SportEventManagementSystem/SportEventManagementSystem/Models/EventViewModels/JoinEventViewModel.cs
@@ -8,16 +8,16 @@ namespace SportEventManagementSystem.Models.EventViewModels
     public class JoinCompetitionViewModel
     { 
         public Competition Competition { get; set; }
-        public string TeamName { get; set; }
-        public List<TeamMemberValidationModel> members { get; set; }
-    }
 
-    public class TeamMemberValidationModel
-    {
+        public string TeamName { get; set; }
+
+        [CustomAttributes.IsListCountLargerThan(1, "JoinIndividual",false,"Please enter atleast one team member to register as a team.")]
+        public String[] TeamMembers { get; set; }
+
         [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Team Member Name")]
-        [StringLength(70, ErrorMessage = "Your {0} must be at max {1} character long", MinimumLength = 1)]
-        public string MemberName { get; set; }
+        public bool JoinIndividual { get; set; }
+
+        [Display(Name = "Are you participating in the team?")]
+        public bool ManagerParticipation { get; set; }
     }
 }
